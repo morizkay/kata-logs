ac468678972fb3a7d2908b559f32a03e8794426d
morizkay <morizkay@live.com>
2023-10-11 13:49:17 +0800

    Put hjandler for Cnjured Mana

--- gilded-rose/typescript/test/golden-master-text-test.ts
+++ gilded-rose/typescript/test/golden-master-text-test.ts
@@ -1,30 +1,31 @@
 import { Item, GildedRose } from '../app/gilded-rose';
 
 const items = [
-  new Item("+5 Dexterity Vest", 10, 20), //
-  new Item("Aged Brie", 2, 0), //
-  new Item("Elixir of the Mongoose", 5, 7), //
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new Item("+5 Dexterity Vest", 5, 20),
+  new Item("Aged Brie", 5, 20),
+  new Item("Elixir of1 the Mongoose", 1, 7),
+  new Item("Sulfuras, Hand of Ragnaros", 5, 80),
+  new Item("Sulfuras, Hand of Ragnaros", 5, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 30),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30),
   // this conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6)];
+  new Item("Conjured Mana Cake", 5, 10)
+];
 
 
 const gildedRose = new GildedRose(items);
 
-let days: number = 2;
+let days: number = 10;
 if (process.argv.length > 2) {
     days = +process.argv[2];
   }
 
 for (let i = 0; i < days; i++) {
   console.log("-------- day " + i + " --------");
-  console.log("name, sellIn, quality");
+  console.log("Name - Sellin - Quality");
   items.forEach(element => {
-    console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
+    console.log(element.name + ' , ' + element.sellIn + ' , ' + element.quality);
 
   });
   console.log();
